a88249f34f76640b72afb20991e8c879ae77267a
tomaszduda1990 <tomasz.duda.krk@gmail.com>
2019-03-03 18:28:45 +0100

    finished mobile markup

--- webpack.config.js
+++ webpack.config.js
@@ -4,56 +4,56 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		path: path.resolve(__dirname, 'dist'),
-		filename: 'bundle.js',
-	},
-	module: {
-		rules: [
-			{
-				exclude: /node_modules/,
-				test: /\.scss$/,
-				use: ExtractTextPlugin.extract({
-					fallback: 'style-loader',
-					use: ['css-loader', 'sass-loader'],
-				}),
-			},
-			{
-				exclude: /node_modules/,
-				test: /\.js$/,
-				use: {
-					loader: 'babel-loader',
-					options: {
-						presets: ['@babel/preset-env'],
-					},
-				},
-			},
-			{
-				exclude: /node_modules/,
-				test: /\.(jpe?g|gif|png|svg)$/,
-				use: [
-					{
-						loader: 'file-loader',
-						options: {
-							name: '[name].[ext]',
-							outputPath: 'assets/',
-							publicPath: 'assets/',
-						},
-					},
-				],
-			},
-			{
-				test: /\.html$/,
-				use: ['html-loader'],
-			},
-		],
-	},
-	plugins: [
-		new ExtractTextPlugin('styles.css'),
-		new HTMLWebpackPlugin({
-			template: 'src/index.html',
-		}),
-		new CleanWebpackPlugin(['dist']),
-	],
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        exclude: /node_modules/,
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: ['css-loader', 'sass-loader']
+        })
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.js$/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      },
+      {
+        exclude: /node_modules/,
+        test: /\.(jpe?g|gif|png|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'assets/',
+              publicPath: 'assets/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: ['html-loader']
+      }
+    ]
+  },
+  plugins: [
+    new ExtractTextPlugin('styles.css'),
+    new HTMLWebpackPlugin({
+      template: 'src/index.html'
+    }),
+    new CleanWebpackPlugin(['dist'])
+  ]
 };
