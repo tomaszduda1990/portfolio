99a9593064efa88d9365e1a273abfa3a27fca5a5
tomaszduda1990 <tomasz.duda.krk@gmail.com>
2019-03-18 21:22:58 +0100

    added animation to abuot me section

--- src/index.js
+++ src/index.js
@@ -1,7 +1,36 @@
 import './styles/main.scss';
 import App from './scripts/App';
 
+const arrToSpan = arr => {
+  const newArr = [];
+  while (arr.length) {
+    const textArr = arr.splice(0, 6);
+    newArr.push(textArr);
+  }
+  return newArr.map(text => {
+    const span = document.createElement('span');
+    span.textContent = text.join(' ');
+    span.textContent += ' ';
+    return span;
+  });
+};
+const textToSpan = p => {
+  const fragment = document.createDocumentFragment();
+  const array = arrToSpan(p.textContent.split(' '));
+  p.innerText = '';
+  array.forEach(el => {
+    fragment.appendChild(el);
+  });
+  p.appendChild(fragment);
+};
 const app = new App(document.querySelectorAll('section'));
 window.onload = () => {
   app.init();
+  const arr = document.querySelectorAll('.about_me__card p');
+  arr.forEach(textToSpan);
 };
+
+const div = document.querySelector('.about_me__card--active');
+div.addEventListener('click', function(e) {
+  e.target.classList.toggle('about_me__card--active');
+});
