57f54b3b56fec2748b5aa932185fb0972e8b5931
tomaszduda1990 <tomasz.duda.krk@gmail.com>
2019-04-02 19:24:13 +0200

    adding infinite scroll to projects

--- src/index.js
+++ src/index.js
@@ -25,16 +25,6 @@ const textToSpan = p => {
   p.appendChild(fragment);
 };
 const app = new App(document.querySelectorAll('section'));
-window.onload = () => {
-  app.init();
-  const arr = document.querySelectorAll('.about_me__card p');
-  arr.forEach(textToSpan);
-};
-
-const div = document.querySelector('.about_me__card--active');
-div.addEventListener('click', function(e) {
-  e.target.classList.toggle('about_me__card--active');
-});
 
 //  ==============================   infinite scroll projects
 
@@ -60,10 +50,60 @@ const projects = [
     live: '#'
   }
 ];
-
-const listItemsArray = projects.map(el => {
+const createLi = el => {
+  const li = document.createElement('li');
   const button = document.createElement('button');
-  const div = document.createElement('div');
+  const links = document.createElement('div');
+  const code = document.createElement('a');
+  const live = document.createElement('a');
+  // prepare li
+  li.classList.add('projects__item');
+  // prepare button
   button.classList.add('button');
   button.textContent = el.name;
+  // prepare single project
+  links.classList.add('project');
+  code.setAttribute('href', el.code);
+  code.setAttribute('target', '_blank');
+  code.classList.add('project__link');
+  live.setAttribute('href', el.live);
+  live.setAttribute('target', '_blank');
+  live.classList.add('project__link');
+
+  // appending elements to each other
+  links.appendChild(code);
+  links.appendChild(live);
+  li.appendChild(button);
+  li.appendChild(links);
+  return li;
+};
+const appendFragment = (projectsList, element) => {
+  const frag = document.createDocumentFragment();
+  projectsList.map(createLi).forEach(el => frag.appendChild(el));
+  element.appendChild(frag);
+};
+const insertBeforeFragment = (projectsList, element) => {
+  const first = element.firstElementChild;
+  const frag = document.createDocumentFragment();
+  projectsList.map(createLi).forEach(el => frag.appendChild(el));
+  element.insertBefore(frag, first);
+};
+// events
+const projectList = document.querySelector('.projects__list ul');
+console.log(projectList);
+window.onload = () => {
+  app.init();
+  const arr = document.querySelectorAll('.about_me__card p');
+  arr.forEach(textToSpan);
+
+  // testing adding li elements to ul onload
+
+  appendFragment(projects, projectList);
+  insertBeforeFragment(projects, projectList);
+};
+
+// test event
+const div = document.querySelector('.about_me__card--active');
+div.addEventListener('click', function(e) {
+  e.target.classList.toggle('about_me__card--active');
 });
