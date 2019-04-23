8ba785294589f69398baab515001d01ab440b6ed
tomaszduda1990 <tomasz.duda.krk@gmail.com>
2019-04-23 21:57:05 +0200

    little corrections

--- src/scripts/components/scrollControl.js
+++ src/scripts/components/scrollControl.js
@@ -3,6 +3,7 @@ import throttle from 'lodash/throttle';
 export default class Scroll {
   constructor(app) {
     this.app = app;
+    this.currentPage = 1;
     this.showPageHandler = this.showPageHandler.bind(this);
   }
 
@@ -10,21 +11,27 @@ export default class Scroll {
     this.app.pos = e.pageY;
     const sectionsHeights = [].slice
       .call(document.querySelectorAll('section'))
-      .map(item => item.offsetTop);
+      .map(item => item.offsetTop - 150);
     this.pageControl(sectionsHeights, this.app.pos);
   }
 
   pageControl(boundries, pos) {
-    if (pos > boundries[0] - 100 && pos < boundries[1] - 100) {
+    // console.log(boundries, pos);
+    if (pos > boundries[0] && pos < boundries[1] && this.currentPage !== 1) {
       console.log('PAGE 1');
-    } else if (pos > boundries[1] - 100 && pos < boundries[2] - 100) {
+      this.currentPage = 1;
+    } else if (pos > boundries[1] && pos < boundries[2] && this.currentPage !== 2) {
       console.log('PAGE 2');
-    } else if (pos > boundries[2] - 100 && pos < boundries[3] - 100) {
+      this.currentPage = 2;
+    } else if (pos > boundries[2] && pos < boundries[3] && this.currentPage !== 3) {
       console.log('PAGE 3');
-    } else if (pos > boundries[3] - 100 && pos < boundries[4] - 100) {
+      this.currentPage = 3;
+    } else if (pos > boundries[3] && pos < boundries[4] && this.currentPage !== 4) {
       console.log('PAGE 4');
-    } else if (pos > boundries[4] - 100 && pos < boundries[5] - 100) {
+      this.currentPage = 4;
+    } else if (pos > boundries[4] && this.currentPage !== 5) {
       console.log('PAGE 5');
+      this.currentPage = 5;
     }
   }
 
